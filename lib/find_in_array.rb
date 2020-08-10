c1a52955b800eea23ef73a9cfefdbbb68e0a9b6e
Noah Eakin <noaheakin@gmail.com>
2020-08-10 17:09:42 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,7 @@
 def find_element_index (array, value_to_find)
   counter = 0
-  while counter < array.length do
+  while array[counter] do
     if array[counter] == value_to_find
-      found_value = counter
       return counter
     end
     counter += 1
